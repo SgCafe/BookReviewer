2d2c3705be801c688a683bd5c43e43f6e66e4471
Sergio <sgabriel.cafe@gmail.com>
2024-11-12 23:06:51 -0300

    :sparkles: feat: adicionando começo de relacionamento

--- BookReviewer.Core/Entities/Review.cs
+++ BookReviewer.Core/Entities/Review.cs
@@ -8,5 +8,7 @@
         public int IdUser { get; set; }
         public int IdBook { get; set; }
         public DateTime CreationDate { get; set; }
+
+        public Book Book { get; set; }
     }
 }
---
+++ BookReviewer.Infrastructure/Persistence/BookReviewerDbContext.cs
@@ -0,0 +1,42 @@
+﻿using BookReviewer.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace BookReviewer.Infrastructure.Persistence
+{
+    public class BookReviewerDbContext : DbContext
+    {
+        public BookReviewerDbContext(DbContextOptions<BookReviewerDbContext> options)
+            : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Book>(e =>
+            {
+                e.HasKey(b => b.Id);
+
+                e.HasMany(b => b.Reviews)
+                    .WithOne(r => r.Book)
+                    .HasForeignKey(r => r.IdBook)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                //Não pode ter mapeamento dentro do Reviewr do book pq a classe dominanete é o Book
+                //Sem book não tem review, logo mapeia apenas aqui para n ter duplicidade
+            });
+
+            builder.Entity<Review>(e =>
+            {
+                e.HasKey(b => b.Id);
+
+                e.
+            });
+            
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
